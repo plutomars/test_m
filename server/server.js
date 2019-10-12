95f8e24eb0ea09b5159e101d92a7a2598725d255
Derek Lu <dereklu0529@gmail.com>
2019-10-12 00:41:19 -0700

    mongolab

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ app.use(bodyParser.json())
 //DB
 const mongoose = require('mongoose')
 mongoose.Promise=global.Promise
-mongoose.connect('mongodb://localhost:27017/book_db')
+
+
+// mongoose.connect('mongodb://localhost:27017/book_db')
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost:27017/book_db')
+
 const {Book} = require('./models/books')
 const {Store} = require('./models/stores')
 
